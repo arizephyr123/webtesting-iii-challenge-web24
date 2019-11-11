e16364c0e1f4d0fafb0146521cd399d8ef3b8a62
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-11 14:25:25 -0800

    Initial Commit, first snapshot test in Dashboard.spec.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from 'react';
+// * includes render from @testing-lib/react
+import * as rtl from '@testing-library/react';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+afterEach(rtl.cleanup);
+
+test("Dashboard renders correctly", () =>{
+const wrapper = rtl.render(<Dashboard/>);
+expect(wrapper.baseElement).toMatchSnapshot();
+});
+
