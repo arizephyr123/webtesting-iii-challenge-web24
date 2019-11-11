9d932a4b6470ab43e6870c5ca103bdd5c81c7f7f
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-11 15:34:19 -0800

    Dashboard.spec.js tests completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,6 +3,7 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import 'jest-dom/extend-expect';
 import Dashboard from './Dashboard';
+import { get } from 'https';
 afterEach(rtl.cleanup);
 
 test("Dashboard renders correctly", () =>{
@@ -10,3 +11,14 @@ const wrapper = rtl.render(<Dashboard/>);
 expect(wrapper.baseElement).toMatchSnapshot();
 });
 
+//tests that Display component rendered and defaults to open and unlocked
+test("Display component rendered, defaults open and unlocked", () => {
+    const { getByText } = rtl.render(<Dashboard/>);
+    expect(getByText(/unlocked/i));
+    expect(getByText(/open/i));
+});
+
+test("Controls component is rendered", () => {
+const { getAllByText } = rtl.render(<Dashboard/>);
+expect(getAllByText(/gate/i)).toHaveLength(2);
+});
